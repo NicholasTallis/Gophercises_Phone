545868fa5d3c5dfb6500284832f6d7ca00a949b8
NicholasTallis <ntallis05@gmail.com>
2022-06-03 10:32:01 -0700

    Init for Messages

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/NicholasTallis/Gophercises_Phone
 
 go 1.18
 
-require github.com/lib/pq v1.10.6 // indirect
+require (
+	crawshaw.io/sqlite v0.3.2 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/golift/imessage v2.1.0+incompatible // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/mattn/go-sqlite3 v1.14.13 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
+	"os"
 	"regexp"
+	"strings"
 
-	phonedb "github.com/NicholasTallis/Gophercises_Phone/db"
-
+	"github.com/golift/imessage"
+	_ "github.com/golift/imessage"
 	_ "github.com/lib/pq"
 )
 
@@ -18,38 +21,80 @@ const (
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
-	must(phonedb.Reset("postgres", psqlInfo, dbname))
 
-	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	must(phonedb.Migrate("postgres", psqlInfo))
+	println("Beginning")
+	iChatDBLocation := "/Users/nicholas/Library/Messages/chat.db"
+	//iChatDBLocation := "/Users/nicholas/Desktop/chat.db"
+	c := &imessage.Config{
+		SQLPath:   iChatDBLocation,                               // Set this correctly
+		QueueSize: 10,                                            // 10-20 is fine. If your server is super busy, tune this.
+		Retries:   3,                                             // run the applescript up to this many times to send a message. 3 works well.
+		DebugLog:  log.New(os.Stdout, "[DEBUG] ", log.LstdFlags), // Log debug messages.
+		ErrorLog:  log.New(os.Stderr, "[ERROR] ", log.LstdFlags), // Log errors.
+	}
+	s, err := imessage.Init(c)
+	checkErr(err)
 
-	db, err := phonedb.Open("postgres", psqlInfo)
-	must(err)
-	defer db.Close()
+	done := make(chan imessage.Incoming) // Make a channel to receive incoming messages.
+	s.IncomingChan(".*", done)           // Bind to all incoming messages.
+	err = s.Start()                      // Start outgoing and incoming message go routines.
+	checkErr(err)
+	log.Print("waiting for msgs")
 
-	err = db.Seed()
-	must(err)
-
-	phones, err := db.AllPhones()
-	must(err)
-	for _, p := range phones {
-		fmt.Printf("Working on... %+v\n", p)
-		number := normalize(p.Number)
-		if number != p.Number {
-			fmt.Println("Updating or removing...", number)
-			existing, err := db.FindPhone(number)
-			must(err)
-			if existing != nil {
-				must(db.DeletePhone(p.ID))
-			} else {
-				p.Number = number
-				must(db.UpdatePhone(&p))
-			}
+	for msg := range done { // wait here for messages to come in.
+		if len(msg.Text) < 60 {
+			log.Println("id:", msg.RowID, "from:", msg.From, "attachment?", msg.File, "msg:", msg.Text)
 		} else {
-			fmt.Println("No changes required")
+			log.Println("id:", msg.RowID, "from:", msg.From, "length:", len(msg.Text))
+		}
+		if strings.HasPrefix(msg.Text, "Help") {
+			// Reply to any incoming message that has the word "Help" as the first word.
+			s.Send(imessage.Outgoing{Text: "no help for you", To: msg.From})
+		}
+		if strings.HasPrefix(msg.Text, "test") {
+			s.Send(imessage.Outgoing{Text: "Auto Reply", To: msg.From})
+		}
+		if strings.HasPrefix(msg.Text, "Test") {
+			s.Send(imessage.Outgoing{Text: "Auto Reply 2", To: msg.From})
 		}
 	}
+
+	//s.Send(imessage.Outgoing{Text: "no help for you", To: "+12038327601"})
+	//println("Should have sent")
+	/*
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+		must(phonedb.Reset("postgres", psqlInfo, dbname))
+
+		psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+		must(phonedb.Migrate("postgres", psqlInfo))
+
+		db, err := phonedb.Open("postgres", psqlInfo)
+		must(err)
+		defer db.Close()
+
+		err = db.Seed()
+		must(err)
+
+		phones, err := db.AllPhones()
+		must(err)
+		for _, p := range phones {
+			fmt.Printf("Working on... %+v\n", p)
+			number := normalize(p.Number)
+			if number != p.Number {
+				fmt.Println("Updating or removing...", number)
+				existing, err := db.FindPhone(number)
+				must(err)
+				if existing != nil {
+					must(db.DeletePhone(p.ID))
+				} else {
+					p.Number = number
+					must(db.UpdatePhone(&p))
+				}
+			} else {
+				fmt.Println("No changes required")
+			}
+		}
+	*/
 }
 
 func must(err error) {
@@ -57,10 +102,30 @@ func must(err error) {
 		panic(err)
 	}
 }
-
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 func normalize(phone string) string {
 	re := regexp.MustCompile("\\D")
 	return re.ReplaceAllString(phone, "")
+}
+
+func displayStudents(db *sql.DB) {
+	row, err := db.Query("SELECT * FROM student ORDER BY name")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var id int
+		var code string
+		var name string
+		var program string
+		row.Scan(&id, &code, &name, &program)
+		log.Println("Student: ", code, " ", name, " ", program)
+	}
 }
 
 // func normalize(phone string) string {
